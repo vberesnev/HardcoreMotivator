08e9fb2f436496f31ebda8a917360e451376fdab
vberesnev <vberesnev@spotware.com>
2020-09-01 22:39:02 +0300

    Data base manager and parser

--- HardcoreMotivator.BL/DataBaseManager.cs
+++ HardcoreMotivator.BL/DataBaseManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SQLite;
@@ -21,6 +22,71 @@ namespace HardcoreMotivator.BL
                 throw new Exception($"Can not get data from database: {e.Message}");
             }
             
+        }
+
+        public static object[] GetTopOneRow(string tableName)
+        {
+            try
+            {
+                using var connection = new SQLiteConnection(GetConnectionString());
+                connection.Open();
+                var command = new SQLiteCommand($"SELECT TOP 1 * FROM {tableName}", connection);
+                var reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    var result = new object[reader.FieldCount];
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            result[i] = reader.GetValue(i);
+                        }
+                    }
+                    return result;
+                }
+                else
+                {
+                    return new object[0];
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Can not get data from database: {e.Message}");
+            }
+        }
+
+        public static List<object[]> GetAllRows(string tableName)
+        {
+            try
+            {
+                using var connection = new SQLiteConnection(GetConnectionString());
+                connection.Open();
+                var command = new SQLiteCommand($"SELECT * FROM {tableName}", connection);
+                var reader = command.ExecuteReader();
+                var list = new List<object[]>();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var row = new object[reader.FieldCount];
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[i] = reader.GetValue(i);
+                        }
+                        list.Add(row);
+                    }
+                }
+                return list;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Can not get data from database: {e.Message}");
+            }
+        }
+
+        public static bool CreateUser(string name, string password)
+        {
+            //TODO
         }
 
         private static string GetConnectionString(string id="Default")
---
+++ HardcoreMotivator.BL/DataBaseParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HardcoreMotivator.BL.Models;
+
+namespace HardcoreMotivator.BL
+{
+    class DataBaseParser
+    {
+        public User GetUser()
+        {
+            try
+            {
+                var sqlResult = DataBaseManager.GetTopOneRow("User");
+                var user = new User(Convert.ToInt32(sqlResult[0]),
+                                    sqlResult[1].ToString(),
+                                    sqlResult[2].ToString());
+                return user;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Can not get user from database: {e.Message}");
+            }
+        }
+
+        public bool CreateUser(User user)
+        {
+            try
+            {
+                DataBaseManager.
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Can not create user: {e.Message}");
+            }
+        }
+    }
+}
