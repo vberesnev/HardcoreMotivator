8871db0d0a671266bb48690ce2ecd2442a109cbe
vberesnev <vberesnev@spotware.com>
2020-09-01 22:38:49 +0300

    Empty view models

---
+++ HardcoreMotivator.GUI/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using HardcoreMotivator.GUI.Annotations;
+
+namespace HardcoreMotivator.GUI.ViewModels
+{
+    internal class BaseViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ HardcoreMotivator.GUI/ViewModels/InitialViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HardcoreMotivator.GUI.ViewModels
+{
+    class InitialViewModel: BaseViewModel
+    {
+    }
+}
